5727f5a3d70767e684626e01a4c7c0cb805a7f19
jerluc <me@jerluc.com>
2018-12-20 12:01:16 -0800

    Filter OSM features to buildings only

--- exporter.sh
+++ exporter.sh
@@ -11,19 +11,26 @@ fi
 GCS_INPUT=$1
 GCS_OUTPUT=$2
 
+COMPRESS_OUTPUT=${COMPRESS_OUTPUT:-"0"}
 INPUT_FILENAME=$(basename $GCS_INPUT)
 OUTPUT_FILENAME=$(basename $GCS_OUTPUT)
+INTER_FILENAME="buildings-$INPUT_FILENAME"
+
+function cleanup {
+    rm -f "$OUTPUT_FILENAME.gz"
+    rm -f $OUTPUT_FILENAME
+    rm -f $INTER_FILENAME
+    rm -f $INPUT_FILENAME
+}
+trap cleanup EXIT
 
 gsutil cp $GCS_INPUT $INPUT_FILENAME
-osmium export -ren -o $OUTPUT_FILENAME -f geojsonseq $INPUT_FILENAME
+osmium tags-filter -o $INTER_FILENAME $INPUT_FILENAME w/building
+osmium export -ren -o $OUTPUT_FILENAME -f geojsonseq $INTER_FILENAME
 
 if [ "$COMPRESS_OUTPUT" -eq "1" ]; then
     gzip $OUTPUT_FILENAME
     gsutil cp "$OUTPUT_FILENAME.gz" "$GCS_OUTPUT.gz"
-    rm "$OUTPUT_FILENAME.gz"
 else
     gsutil cp $OUTPUT_FILENAME $GCS_OUTPUT
-    rm $OUTPUT_FILENAME
 fi
-
-rm $INPUT_FILENAME
